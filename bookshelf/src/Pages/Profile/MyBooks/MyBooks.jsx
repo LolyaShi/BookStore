aa6246531d22440c190eb73cbb1ad8fc74a3fb39
Olya <116220190+LolyaShi@users.noreply.github.com>
2024-12-26 23:27:40 +0200

    add some styles

--- bookshelf/src/Pages/Profile/MyBooks/MyBooks.jsx
+++ bookshelf/src/Pages/Profile/MyBooks/MyBooks.jsx
@@ -12,27 +12,36 @@ export default function MyBook(){
     }, [])
     console.log(myBooks)
 
-    return(
-        <div className="MyBooks">
-            <h1>My books</h1>
-            <ul className="book-list">
-                {myBooks.map(item => {
-                    return (
-                        <li className="item" key={item.id}>
-                            <div className="item-content">
-                                 <Link to={`/categories/${item.path}/${item.id}`}>{item.title}</Link>
-                                 <div>
-                                     <div className="price">{item.price}</div>
-                                    
-                                 </div>
-                                           
-                            </div>
+    if(myBooks){
+        return(
+            <div className="MyBooks">
+                <h1>My books</h1>
+                <ul className="book-list">
+                    {myBooks.map(item => {
+                        return (
+                            <li className="item" key={item.id}>
+                                <div className="item-content">
+                                    <Link to={`/categories/${item.path}/${item.id}`}>{item.title}</Link>
+                                    <div>
+                                        <div className="price">{item.price}</div>
                                         
-                         </li>
-                    )
-                })}
-            </ul>
-           
-        </div>
-    )
+                                    </div>
+                                            
+                                </div>
+                                            
+                            </li>
+                        )
+                    })}
+                </ul>
+            
+            </div>
+        )
+    }
+    else{
+        return(
+            <div className="MyBooks">
+                <h1>You don't buy books yet</h1>
+            </div>
+        )
+    }
 }
